9368d91c4af36838caaa350e921fde65e17951fc
Lupin <lukasgtx71@gmail.com>
2024-11-28 13:00:31 +0100

    Dodano silnie

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+int Silnia(int a) {
+	if (a == 0) { return 1; }
+
+	int wynik = 1;
+	for (int i = 1; i <= a; i++) {
+		wynik *= i;
+	}
+
+	return wynik;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
@@ -12,8 +23,13 @@ int main() {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+	cout << "1. Silnia" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+
+	if (wyjscie == 1) { 
+		cout << "Silnie wprowadzynch liczb:" << endl << Silnia(a) << endl << Silnia(b) << endl;
+	}
     } while(wyjscie != 0);
     return 0;
 }
