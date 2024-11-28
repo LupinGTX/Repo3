b429c47b64c2c1eb3c7796300c43a3fe51f936b8
Lupin <lukasgtx71@gmail.com>
2024-11-28 13:17:53 +0100

    Dodano wywolanie funkcji liczby pierwszej

--- projekt1.cpp
+++ projekt1.cpp
@@ -15,26 +15,38 @@ bool Pierwsza(int a) {
 		return false;
 	}
 
-	for (int i = 1; i <= (a/2); i += 2) {
+	for (int i = 3; i <= (a/2); i += 2) {
 		if (a % i == 0) {
-			return true;
+			return false;
 		}
+	}
 
-	return false;
+	return true;
 }
 
 int main() {
-    int a, b;
-    cout << "Podaj liczbe calkowita ";
-    cin >> a >> b;
+    int a;
+    cout << "Podaj liczbe calkowita: ";
+    cin >> a;
     int wyjscie;
     do {
         cout << endl;
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+	cout << "2. Liczba pierwsza" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+
+	if (wyjscie == 2) {
+		if (Pierwsza(a)) {
+			cout << "Podana przez uzytkownika liczba jest pierwsza." << endl;
+		}
+		else {
+			cout << "Podana przez uzytkownika liczba NIE jest pierwsza." << endl;
+		}
+	}
+
     } while(wyjscie != 0);
     return 0;
 }
