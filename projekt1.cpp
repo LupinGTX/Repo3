fda538a695b1ba7536dcc8bb82c0dedf888ee00d
Lupin <lukasgtx71@gmail.com>
2024-11-28 13:11:25 +0100

    Dodano funkcje liczby pierwszej

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+bool Pierwsza(int a) {
+	if (a == 1) {
+		return false;
+	}
+
+	if (a == 2) {
+		return true;
+	}
+
+	if (a % 2 == 0) {
+		return false;
+	}
+
+	for (int i = 1; i <= (a/2); i += 2) {
+		if (a % i == 0) {
+			return true;
+		}
+
+	return false;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
